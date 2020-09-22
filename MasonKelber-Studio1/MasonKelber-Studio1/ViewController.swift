a794bf319f7d81cd38bb488d4fb930b4e9e1a6cb
MKelber <54511853+MKelber@users.noreply.github.com>
2020-09-22 09:26:00 -0500

    Add files via upload

---
+++ MasonKelber-Studio1/MasonKelber-Studio1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MasonKelber-Studio1
+//
+//  Created by Mason Kelber on 9/22/20.
+//  Copyright © 2020 Mason Kelber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
